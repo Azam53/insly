9bea2c12528424c5071193d57bf70fd5c48e24d4
Azam <aliazam005@gmail.com>
2018-09-11 12:50:54 +0530

    formatting insurance class

--- insurance/insurance.php
+++ insurance/insurance.php
@@ -1,22 +1,13 @@
 <?php
 class Insurance 
 {
-  // // only accessible *inside* of the class
-  // public $amount,$rate,$installments;
 
-  // // accessible *outside* of the class
-  // public  $basePremiumfigure ;
-  // public  $commissionfigure;
-  // public  $taxfigure;
-  // public  $basePremium,$commission,$tax,$totalCost;
-  // public  $installmentBasePremium, $installmentcommission ,$installmenttax,$installmenttotalCost;
 
   public function index()
   {
      include 'views/index.php';
   }
 
-  
 }
 
 // Calling the index function for landing page to start 
